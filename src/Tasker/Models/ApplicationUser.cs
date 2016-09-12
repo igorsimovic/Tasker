9e53ad762dfce4bab265adcd558e482cdea06dc8
milos.milovanovic@comtrade.com <milos.milovanovic@comtrade.com>
2016-09-12 10:43:54 +0200

    Controllers restore

--- src/Tasker/Models/ApplicationUser.cs
+++ src/Tasker/Models/ApplicationUser.cs
@@ -10,5 +10,7 @@ namespace Tasker.Models
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public object PasswordHash { get; internal set; }
+        public string Email { get; internal set; }
     }
 }
